1b98aa03e689b83e69426d2d5039a66d6d597ab1
Jayalls <jason.allais@outlook.com>
2023-04-05 15:19:47 +0200

    first commit + initialization project + creating Title

---
+++ mytodo/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.scss'
+import Title from './component/Title/Title'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Title />
+    </div>
+  )
+}
+
+export default App
---
+++ mytodo/src/component/Title/Title.jsx
@@ -0,0 +1,13 @@
+import './style.scss'
+
+function Title() {
+
+  return (
+    <div className="title">
+      <span>MY TO DO LIST</span>
+      <h1>WORK HARD</h1>
+    </div>
+  )
+}
+
+export default Title;
