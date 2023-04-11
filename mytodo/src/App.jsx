7418b56e7b5b3df01e532793767c2879da3ea3c0
Jayalls <jason.allais@outlook.com>
2023-04-11 14:34:22 +0200

    Add category add + edit + delete

--- mytodo/src/App.jsx
+++ mytodo/src/App.jsx
@@ -2,20 +2,38 @@ import './App.scss';
 import Title from './component/Title/Title';
 import CategoryBar from './component/CategoryBar/CategoryBar';
 import TodoList from './component/TodoList/TodoList';
+
 import { useState } from 'react';
 
 function App() {
-  const categories = ["Task 1", "Task 2", "Task 3", "Task 4"]
-  const [tasks , setTasks] = useState([])
+  const [categories, setCategories] = useState(["Task 1"]);
+  const [tasks , setTasks] = useState([]);
+
+  const addCategory = (category) => {
+    setCategories([...categories, category]);
+  };
+
+  const deleteCategory = (index) => {
+    const newCategories = [...categories];
+    newCategories.splice(index, 1);
+    setCategories(newCategories);
+  };
+
+  const editCategory = (index, newCategory) => {
+    const newCategories = [...categories];
+    newCategories[index] = newCategory;
+    setCategories(newCategories);
+  };
 
   return (
     <div className="App">
       <Title />
-      <CategoryBar categories={categories} />
+      <CategoryBar categories={categories} addCategory={addCategory} deleteCategory={deleteCategory} editCategory={editCategory} />
       <TodoList tasks={tasks} setTasks={setTasks} />
     </div>
   );
 }
+
 
 export default App;
 
--- mytodo/src/component/CategoryBar/CategoryBar.jsx
+++ mytodo/src/component/CategoryBar/CategoryBar.jsx
@@ -1,16 +1,57 @@
 import "./style.scss";
+import { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPen } from '@fortawesome/free-solid-svg-icons'
 
-function CategoryBar({categories}) {
+function CategoryBar({ categories, addCategory, deleteCategory, editCategory }) {
+  const [editing, setEditing] = useState(false);
+  const [newCategory, setNewCategory] = useState("");
 
-    return (
-        <div className="category-bar">
-        {categories.map((category, index) => (
-          <div className="category" key={index}>
-            {category}
-          </div>
-        ))}
+  const handleAddCategory = () => {
+    addCategory(newCategory);
+    setNewCategory("");
+    setEditing(false);
+  };
+
+  const handleEditCategory = (index) => {
+    editCategory(index, newCategory);
+    setNewCategory("");
+    setEditing(false);
+  };
+
+  return (
+    <div className="category-bar">
+      {categories.map((category, index) => (
+        <div className="category" key={index}>
+          {editing === index ? (
+            <>
+              <input type="text" value={newCategory} onChange={(e) => setNewCategory(e.target.value)} />
+              <button onClick={() => handleEditCategory(index)}>Save</button>
+              <button onClick={() => setEditing(false)}>X</button>
+            </>
+          ) : (
+            <>
+              {category}
+              <button onClick={() => setEditing(index)}><FontAwesomeIcon icon={faPen}/></button>
+              <button onClick={() => deleteCategory(index)}>X</button>
+            </>
+          )}
+        </div>
+      ))}
+      <div className="category-edit">
+        {editing === "new" ? (
+          <>
+            <input type="text" value={newCategory} onChange={(e) => setNewCategory(e.target.value)} />
+            <button onClick={handleAddCategory}>Add</button>
+            <button onClick={() => setEditing(false)}>X</button>
+          </>
+        ) : (
+          <button onClick={() => setEditing("new")}>+</button>
+        )}
       </div>
-    )
-  }
+    </div>
+  );
+}
+
   
   export default CategoryBar
